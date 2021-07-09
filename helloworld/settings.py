5f5e01d3278c93dc5a16ea836349abf27b622d61
saurabh <saurabhdantani09@gmail.com>
2021-07-09 11:42:42 +0530

    first

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-django_heroku.settings('locals')
+# django_heroku.settings('locals')
 
 STATICFILES_DIRS=[
 
@@ -134,3 +134,4 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
